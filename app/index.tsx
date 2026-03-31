a39c50214e6f6431aaf72273fab980183333bc76
laviniamarante <laviinia.amarante@gmail.com>
2026-03-31 08:36:56 -0300

    Refactor input handling in Index component and update start script in package.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [nome, setNome] = useState('');
   return (
     <View
       style={{
         flex: 1,
+        padding: 16,
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   >
+      <TextInput
+        placeholder = "Digite Algo"
+        onChangeText={ novoTexto => setNome (novoTexto) }
+        value={nome}
+        style={{
+          width: "100%",
+          padding: 5,
+          borderWidth: 1
+        }}
+      />
+      <TextInput> {nome}</TextInput>
+        </View>
   );
 }
+
