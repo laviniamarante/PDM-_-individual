c748c8173bd01084585bdfaed3a486effd58990f
laviniamarante <laviinia.amarante@gmail.com>
2026-03-31 11:04:29 -0300

    Add layout structure and implement item management in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,80 @@
 import { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, FlatList, Text, TextInput, View } from "react-native";
+import {nextId} from "../util/geral";
 
 export default function Index() {
   const [nome, setNome] = useState('');
+  const [lista, setLista] = useState([]);
+
+
+
+  function adiciona(){
+    setLista([...lista, {id:nextId(lista), nome: nome}]);
+    setNome ('');
+  }
+
+  function remove(id) {
+    setLista( lista.filter( item => item.id!== id ) );
+  }
+
+  function RenderItem({contato}) {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: '#dddddd',
+          width: '100%',
+          padding: 5,
+          alignItems: 'center'
+        }}
+      >
+        <Text style={{flex: 1}}>{contato.nome}</Text>
+        <Button
+          title="-"
+          onPress={ () => remove(contato.id) }
+        />
+      </View>
+    )
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        padding: 16,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        backgroundColor: 'white'
       }}
-   >
-      <TextInput
-        placeholder = "Digite Algo"
-        onChangeText={ novoTexto => setNome (novoTexto) }
-        value={nome}
+    >
+      <View
         style={{
-          width: "100%",
-          padding: 5,
-          borderWidth: 1
+          flexDirection: 'row',
+          width: '100%'
+        }}
+      >
+        <TextInput
+          placeholder="Digite um nome ..."
+          value={nome}
+          onChangeText={ t => setNome(t)}
+          style={{
+            flex: 1,
+            borderWidth: 1,
+            padding: 5
+          }}
+        />
+        <Button 
+          title="+"
+          onPress={ adiciona }
+        />
+      </View>
+      <FlatList
+        data={lista}
+        renderItem={ ({item}) => <RenderItem contato={item} /> }
+        ItemSeparatorComponent={ () => <View style={{ height: 2 }} />}
+        style={{
+          width: '100%'
         }}
       />
-      <TextInput> {nome}</TextInput>
-        </View>
+    </View>
   );
 }
-
